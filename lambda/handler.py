8ebd5ba40a701617ee982b68a73407f911fe59ae
Gabriel110 <gabrielcorreiadev@gmail.com>
2023-10-21 23:49:07 -0300

    read file

--- lambda/handler.py
+++ lambda/handler.py
@@ -1,32 +1,61 @@
+import json
 import logging
-import boto3
-import os
+import uuid
 
+import boto3
+
+from read_s3_file import ReadFileS3
 
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 
-def read_message_s3(local_path):
-  row = []
-  file = open(local_path, 'r')
-  for x in file:
-    row.append(x)
-  return row
+
+def get_file_on_s3(file):
+	row_batch_array = []
+	for row in file.get_csv_reader():
+		row_batch_array.append(row)
+
+	return row_batch_array
+
+
+def invoke_lambda(function_name, event):
+	print("ENVOCADA")
+	payload = json.dumps(event).encode('utf-8')
+	client = boto3.client('lambda')
+	client.invoke(
+		FunctionName=function_name,
+		InvocationType='Event',
+		Payload=payload
+	)
+
+
+def offset_invoke_lambda(offset, file, event, context, correlation_id):
+	new_offset = offset + file.body_lines.offset
+	if new_offset < file.s3_object.content_length:
+		new_event = {
+			**event,
+			"offset": new_offset,
+			"correlation_id": correlation_id
+		}
+		invoke_lambda(context.function_name, new_event)
+	else:
+		file.move_file_to_processed_folder()
+		return
 
 
 def handler(event, context):
+	for record in event['Records']:
+		bucket_name = record['s3']['bucket']['name']
+		object_key = record['s3']['object']['key']
+		correlation_id = event.get('correlation_id', str(uuid.uuid4()))
+		offset = event.get('offset', 0)
 
-  endpoint_url =  'http://%s:4566' % os.environ['LOCALSTACK_HOSTNAME']
-  s3 = boto3.client('s3', endpoint_url=endpoint_url, region_name='us-east-1', verify=False)
+		file = ReadFileS3(bucket_name, object_key, correlation_id, offset)
 
-  for record in event['Records']:
-    bucket_name = record['s3']['bucket']['name']
-    object_key = record['s3']['object']['key']
+		row_batch_array = get_file_on_s3(file)
+		offset_invoke_lambda(offset, file, event, context, correlation_id)
 
-    local_path = os.path.join('/tmp', os.path.basename(object_key))
-    s3.download_file(bucket_name, object_key, local_path)
+		for message in row_batch_array:
+			print(message)
 
-    row_message = read_message_s3(local_path)
-    print(row_message)
-    
-  return event
+		return event
---
+++ lambda/read_s3_file.py
@@ -0,0 +1,73 @@
+import csv
+import os
+
+import boto3
+import botocore
+
+
+class ReadFileS3:
+  endpoint_url = 'http://%s:4566' % os.environ['LOCALSTACK_HOSTNAME']
+  bucket_name: str
+  object_key: str
+  correlation_id: str
+  offset: int
+  s3_resource = boto3.resource('s3', endpoint_url=endpoint_url, region_name='us-east-1', verify=False)
+  s3_object = None
+  body_lines = None
+
+  def __init__(self, bucket_name, object_key, correlation_id, offset):
+    self.offset = offset
+    self.correlation_id = correlation_id
+    self.object_key = object_key
+    self.bucket_name = bucket_name
+    self.s3_object = self.s3_resource.Object(bucket_name=self.bucket_name, key=self.object_key)
+    self.body_lines = self.get_object_body_lines(self.s3_object)
+
+  def get_csv_reader(self):
+    chunk_size = 1024
+    csv.register_dialect('lambda', 'excel', delimiter=';')
+    return csv.DictReader(self.body_lines.iter_lines(chunk_size), fieldnames=self.get_field_names(), dialect='lambda')
+
+  def get_object_body_lines(self, s3_object):
+    try:
+      resp = s3_object.get(Range=f'bytes={self.offset}-')
+      body: botocore.response.StreamingBody = resp['Body']
+      return BodyLines(body)
+    except Exception as e:
+      raise e
+
+  def move_file_to_processed_folder(self):
+    try:
+      s3 = boto3.client('s3', endpoint_url=self.endpoint_url, region_name='us-east-1', verify=False)
+      old_key = str(self.object_key)
+      new_key = 'processados/' + old_key
+      copy_source = {
+        'Bucket': self.bucket_name,
+        'Key': self.object_key
+      }
+      s3.copy_object(CopySource=copy_source, Bucket=self.bucket_name, Key=new_key)
+      s3.delete_object(Bucket=self.bucket_name, Key=self.object_key)
+      return
+    except Exception as e:
+      raise e
+
+  def get_field_names(self):
+    return ['QuotaAmount', 'StartDate', 'OwnerName', 'Username']
+
+
+class BodyLines:
+  def __init__(self, body: botocore.response.StreamingBody, initial_offset=0):
+    self.body = body
+    self.offset = initial_offset
+
+  def iter_lines(self, chunk_size):
+    pending = b''
+    for chunk in self.body.iter_chunks(chunk_size):
+      lines = (pending + chunk).splitlines(True)
+      for line in lines[:-1]:
+        self.offset += len(line)
+        yield line.decode('utf-8')
+      pending = lines[-1]
+    if pending:
+      self.offset += len(pending)
+      yield pending.decode('utf-8')
